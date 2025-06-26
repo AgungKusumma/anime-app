68ddc333c3adea6834c40a7fcbaa7532aadc5ec8
agungkusuma <agungkusuma.id@gmail.com>
2025-06-26 22:51:35 +0700

    security: implement certificate pinning using SHA-256 for secure HTTPS connections

--- core/src/main/java/com/agungkusuma/core/di/NetworkModule.kt
+++ core/src/main/java/com/agungkusuma/core/di/NetworkModule.kt
@@ -6,6 +6,8 @@ import com.agungkusuma.core.utils.Constants.ApiComponents.BASE_URL
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import okhttp3.Cache
+import okhttp3.CertificatePinner
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -26,9 +28,15 @@ val networkModule = module {
     }
 
     single {
+        val hostname = BASE_URL.toHttpUrl().host
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/bdrBhpj38ffhxpubzkINl0rG+UyossdhcBYj+Zx2fcc=")
+            .build()
         OkHttpClient.Builder().cache(get()).addInterceptor(HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
-        }).connectTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS).build()
+        }).connectTimeout(30, TimeUnit.SECONDS).readTimeout(30, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
     }
 
     single<Retrofit> {
