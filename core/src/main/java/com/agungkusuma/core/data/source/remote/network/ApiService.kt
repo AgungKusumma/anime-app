a50bb366f195b9a591db38f6ba201f87e70808dc
agungkusuma <agungkusuma.id@gmail.com>
2025-06-26 13:27:52 +0700

    feat(api): add page query parameter to getAnimeList

--- core/src/main/java/com/agungkusuma/core/data/source/remote/network/ApiService.kt
+++ core/src/main/java/com/agungkusuma/core/data/source/remote/network/ApiService.kt
@@ -6,10 +6,13 @@ import com.agungkusuma.core.utils.Constants.ApiEndpoint.ANIME_DETAIL
 import com.agungkusuma.core.utils.Constants.ApiEndpoint.ANIME_LIST
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @GET(ANIME_LIST)
-    suspend fun getAnimeList(): AnimeListResponse
+    suspend fun getAnimeList(
+        @Query("page") page: Int = 2,
+    ): AnimeListResponse
 
     @GET(ANIME_DETAIL)
     suspend fun getAnimeDetail(
